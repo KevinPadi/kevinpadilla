fa5da63b5c42c12bc3f9c68c8c91ea09d6c1bed0
Kevin <kevpadilla01@gmail.com>
2025-06-06 00:43:03 -0300

    fix: update InfiniteSlider styles for improved visual consistency and remove debug log

--- src/components/infinite-slider-2.tsx
+++ src/components/infinite-slider-2.tsx
@@ -40,13 +40,12 @@ const tech = [
 export default function InfiniteSlider2() {
   return (
     <div className='relative h-40 w-full'>
-      <InfiniteSlider gap={30} speedOnHover={20}  className='relative flex h-full w-full items-center before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-gradient-to-r before:from-neutral-900 before:to-transparent before:content-[""] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-gradient-to-r after:from-neutral-900 after:to-transparent after:content-[""]' >
+      <InfiniteSlider gap={30} speedOnHover={20}  className='relative flex h-full w-full items-center before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-gradient-to-r before:from-neutral-950 before:to-transparent before:content-[""] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-gradient-to-r after:from-neutral-950 after:to-transparent after:content-[""]' >
       {
         tech.map((src, index) => {
-          console.log(src.icon)
           return (
           <div key={index} className="flex flex-col gap-2 items-center justify-center aspect-square">
-            <span className="w-10 sm:w-20 p-2 sm:p-4 bg-neutral-800 rounded-xl aspect-square flex items-center justify-center">
+            <span className="w-10 sm:w-20 p-2 sm:p-4 bg-neutral-900 rounded-xl sm:rounded-2xl aspect-square flex items-center justify-center">
               {src.icon}
             </span>
             <span className="text-sm text-center text-neutral-100">
