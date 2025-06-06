8571560b0ec81c89f6d5f811cb046213dc0506e1
Kevin <kevpadilla01@gmail.com>
2025-06-06 00:31:18 -0300

    feat: add BadgeList component to display icons with labels

---
+++ src/components/badge.tsx
@@ -0,0 +1,47 @@
+// BadgeList.tsx
+import {
+  React,
+  Git,
+  GitHub,
+  Tailwind,
+  JavaScript,
+  Express,
+  Node,
+  TypeScript,
+} from './Icons';
+
+type BadgeListProps = {
+  items: string[];
+};
+
+const icons = [
+  { name: 'react', component: <React /> },
+  { name: 'git', component: <Git /> },
+  { name: 'github', component: <GitHub /> },
+  { name: 'tailwind', component: <Tailwind /> },
+  { name: 'javascript', component: <JavaScript /> },
+  { name: 'express', component: <Express /> },
+  { name: 'node', component: <Node /> },
+  { name: 'typescript', component: <TypeScript /> },
+];
+
+export const BadgeList = ({ items }: BadgeListProps) => {
+  return (
+    <div className="flex flex-wrap gap-2">
+      {items.map((item) => {
+        const icon = icons.find((i) => i.name === item.toLowerCase());
+        console.log(icon)
+        if (!icon) return null;
+        return (
+          <div key={item} className="flex items-center justify-center gap-1 bg-neutral-900 border border-neutral-800 text-white rounded-full px-2 py-1 text-sm pointer-events-none">
+            <div className='size-4 border border-transparent rounded-full flex items-center justify-center'>
+              {icon.component}
+            </div>
+            <span className="capitalize">{item}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
